eb710e5631e6db2b5349b7474c765ff91552e10d
Otim Benjamin Munguriek <benjaminmunguriek@gmail.com>
2023-01-03 23:34:19 +0300

    created tests, some other stuff and some shit

---
+++ config/tests/factories.py
@@ -0,0 +1,27 @@
+import factory
+from django.contrib.auth.models import User 
+
+from blog.models import Post
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        
+    password = 'test123'
+    username = 'test'
+    is_superuser = True
+    is_staff = True
+    
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+        
+    title = 'x'
+    subtitle = 'x'
+    slug = 'x'
+    author = 'x'
+    content = 'x'
+    author = factory.SubFactory(UserFactory)
+    status = 'published'
